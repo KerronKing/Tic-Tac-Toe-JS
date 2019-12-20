61af895f12bb25f10d904b8cb926314830e70e5d
KerronKing <king_of_palmiste@hotmail.com>
2019-12-20 16:37:24 -0400

    Edit player object and add player input form

--- app.js
+++ app.js
@@ -11,10 +11,22 @@ const gameflow = (() => {
 
 // Player object
 const Player = (name, symbol) => {
-  let name = name;
-  let symbol = symbol;
   return { name, symbol }
 };
+
+// Global Array to store player objects
+const players = [];
+
+// Next steps:
+
+// Make the board with divs (3 rows x 3 columns).
+// Each div would have a unique id (as follows: position-${1 - 9} )
+// Each div would have an event listener (click event) that saves an
+  // entry on the gameBoard array.
+// Render textContent (appropriate player symbol) in the div that was clicked.
+// Add function to remove event listener if a game board div has text content already.
+
+
 
 
 

907834210fefce1e37cd1fa48aa504aedf4ff92d
KerronKing <king_of_palmiste@hotmail.com>
2019-12-20 15:56:14 -0400

    Add html skeleton and initialize modules and factory functions

---
+++ app.js
@@ -0,0 +1,20 @@
+// Game-board module
+const gameboard = (() => {
+  let playArea = [];
+  return { playArea };
+})();
+
+// Game-flow module
+const gameflow = (() => {
+
+})();
+
+// Player object
+const Player = (name, symbol) => {
+  let name = name;
+  let symbol = symbol;
+  return { name, symbol }
+};
+
+
+

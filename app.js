2f0129edc28dd5c4d3e3e34145f125376929d53e
KerronKing <king_of_palmiste@hotmail.com>
2019-12-20 18:53:15 -0400

    Add comments for next steps in app logic

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const gameFlow = (() => {
 
   const addPlayer = (name1, name2) => {
 
-  const obj1 = Player(name1, 'X');
-  const obj2 = Player(name2, '0');
+  const obj1 = Player(name1, 'X', 1);
+  const obj2 = Player(name2, '0', 0);
     players.push(obj1, obj2);
   };
 
@@ -27,18 +27,38 @@ const gameFlow = (() => {
     console.log('test');
     });
   };
-    return {getPlayerInfo};
+
+  // const gameWon = () => {
+
+  // }
+
+  // const playerTurn = () => {
+
+  //   while (!gameWon()) {
+  //     if (players.moveNumber % 2 == 1) {
+  //       const boardElements = document.getElementById('board').children;
+  //       boardElements.forEach(elem => {
+  //         elem.addEventListener('click', () => {
+  //           elem.textContent = `${players[i].symbol}`;
+  //         })
+  //       })
+  //     }
+  //   }
+  return {getPlayerInfo};
 })();
 
 // Player object
 const Player = (name, symbol) => {
-  return { name, symbol }
+  let moveNumber = 0;
+  return { name, symbol, moveNumber }
 };
 
 gameFlow.getPlayerInfo();
+
 // Next steps:
 
 // Each div would have an event listener (click event) that saves an
   // entry on the gameBoard array.
 // Render textContent (appropriate player symbol) in the div that was clicked.
 // Add function to remove event listener if a game board div has text content already.
+// runGame function - goes in gameFlow module and is the only (probable) global function call
